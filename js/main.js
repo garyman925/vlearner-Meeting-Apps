1a471bb662388432210716baeaa2accb03c07081
garyman925 <garyman925@gmail.com>
2020-05-07 14:49:38 +0800

    Starting Mobile UI, testing data-type style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 
+$('#test-btn').on('click',function(){
+
+    var wrap = $('.wrapper'),
+        wrapAttr = wrap.attr("data-status");
+
+    if( wrapAttr === "open-half" ){
+        wrap.attr('data-status','open-full');
+    }else{
+        wrap.attr('data-status','open-half');
+    }
+
+})
+
+$(window).resize(function() {
+    
+    var wrap = $('.wrapper'),
+    wrapAttr = wrap.attr("data-status");
+
+    //Screen > 992px
+    if (window.matchMedia('screen and (min-width: 1025px)').matches) {
+        wrap.attr('data-status','open-half');
+    }else{
+        wrap.attr('data-status','open-full');
+    }
+});
